520e0a30a8158ba6402f0cb22d98e40627ebb2c9
morkkes <muratov583@gmail.com>
2018-04-18 00:26:36 +0300

    Пошло
    Дело

--- Algol1.py
+++ Algol1.py
@@ -11,11 +11,58 @@ import math as ma
 from PyQt5 import QtGui, QtCore, QtWidgets
 g=9.8
 RAD = ma.pi/180
-
 PI = ma.pi
+
+class Animation(QtWidgets.QMainWindow) :
+    def __init__(self) :
+        QtWidgets.QMainWindow.__init__(self)
+        self.resize(700, 8000)
+        self.setWindowTitle('Circ')
+        
+        self.buttonStart = QtWidgets.QPushButton("Start",self)
+        self.buttonStart.move(30,160)
+        self.buttonStart.clicked.connect(self.onStart)
+
+        self.buttonStop = QtWidgets.QPushButton("Stop",self)
+        self.buttonStop.move(30,190)
+        self.buttonStop.clicked.connect(self.onStop)       
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.onTimer) 
+        
+        
+    def paintEvent(self,ev) :
+       
+        qp=QtGui.QPainter()
+        qp.begin(self)
+        
+        qp.end()
+
+        
+    def onTimer(self) :
+        
+        self.t+=0.1
+       
+        self.update()
+        
+    def onStop(self) :
+        self.timer.stop()
+        
+app = QtWidgets.QApplication(sys.argv)
+widget = AnimationExample()
+widget.show()
+sys.exit(app.exec_())  
+
+        
+        
+        
+    '''
 class Circulation():
 class Recognition():
+    '''
 
+    
     
     
     
